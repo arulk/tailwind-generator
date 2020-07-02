5e1a4fb3c7898d0326c04789187a08c6e6586045
Arul Kumaravel <arulk@live.com>
2020-07-02 18:34:26 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+
+  // tailwind.config.js
+module.exports = {
+  purge: {
+    enabled:  false,
+    content: [
+      // './src/**/*.html',
+      // './src/**/*.vue',
+      // './src/**/*.jsx',
+    ]
+  },
+
+  theme: {
+
+  },
+
+  variants: {
+
+  },
+  
+  plugins: [
+    require('@tailwindcss/ui')
+  ],
+}
