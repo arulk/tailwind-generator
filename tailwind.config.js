b9f1bd31ddcc10e12d7b6e216e752ba5fcc5c0ec
Arul Kumaravel <arulk@live.com>
2020-07-02 19:17:06 +0800

    Add template html files to list of tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
   // tailwind.config.js
 module.exports = {
   purge: {
-    enabled:  false,
+    // When this flag is enabled to true. We will get purged tailwind.css file with only styles used in files defined
+    // in content list.
+    enabled:  false, 
     content: [
+      './templates/**/*.html'
       // './src/**/*.html',
       // './src/**/*.vue',
       // './src/**/*.jsx',
